a144667fa6d911633d81b5cb6c60f231e4687b66
Shaker Hamdi <sheko.elanteko@gmail.com>
2014-06-09 09:39:40 +0300

    filtered the price range array based on the section value

--- js/priceFilter.js
+++ js/priceFilter.js
@@ -1,5 +1,48 @@
 var app = angular.module('App', []);
 
+var arr = app_data.price_filters;
+    minimum = [];
+    maximum = [];
+
+for($i=0; $i < arr.length; $i++){
+
+    if(arr[$i]['section'] === 2) {
+        minimum.push(arr[$i]['value']);
+    } else if(arr[$i]['section'] === 1) {
+        maximum.push(arr[$i]['value']);
+    }
+}
+
+console.log(minimum);
+console.log(maximum);
+
+
+
+app.controller('MainController', function ($scope) {
+    $scope.mins = minimum;
+    $scope.maxs = maximum;
+    
+    $scope.getMxRange = function () {
+        var arr = [];
+        if ($scope.minVal) {
+            angular.forEach($scope.maxs, function (maxVal) {
+                if ($scope.minVal < maxVal) {
+                    this.push(maxVal);
+                }
+            }, arr);
+        }
+        else {
+            arr = $scope.maxs;
+        }
+        return arr;
+    };
+});
+
+
+
+
+/*var app = angular.module('App', []);
+
 app.controller('MainController', function ($scope) {
     $scope.mins = [100, 200, 300, 400];
     $scope.maxs = [200, 300, 400, 500];
@@ -18,7 +61,7 @@ app.controller('MainController', function ($scope) {
         }
         return arr;
     };
-});
+});*/
 
 
 /*app.controller('MainController', ['$scope', function ($scope) {
